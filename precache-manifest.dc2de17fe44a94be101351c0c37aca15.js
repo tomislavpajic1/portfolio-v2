a8e734cd9c4299d055bccfe2cd57dab91fb5c70c
tomislavpajic1 <tomislav.pajic@protonmail.com>
2022-04-18 22:47:17 +0200

    Updates

---
+++ precache-manifest.dc2de17fe44a94be101351c0c37aca15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09c8adbef3d41627119a4a9e1926d5e",
+    "url": "/portfolio-v2/index.html"
+  },
+  {
+    "revision": "5085db7168cae41f7ff2",
+    "url": "/portfolio-v2/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "b67c6a8ec47224b12663",
+    "url": "/portfolio-v2/static/css/main.137ef0a2.chunk.css"
+  },
+  {
+    "revision": "5085db7168cae41f7ff2",
+    "url": "/portfolio-v2/static/js/2.5348a1dc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio-v2/static/js/2.5348a1dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b67c6a8ec47224b12663",
+    "url": "/portfolio-v2/static/js/main.04b45591.chunk.js"
+  },
+  {
+    "revision": "8aadacf5700fb4c7316c",
+    "url": "/portfolio-v2/static/js/runtime-main.91bd4a58.js"
+  }
+]);
